7c1987e69d33a9066065ad8595a7a733b8e51a9a
MatteoMarcoM <matteomm00@gmail.com>
2025-03-16 17:11:08 +0100

    UI PoA page improved

--- lib/pages/poa_details_page.dart
+++ lib/pages/poa_details_page.dart
@@ -19,7 +19,6 @@ class PoADetailsPage extends StatelessWidget {
   final String engagementData;
   final Map<String, String> sensitiveDataHashMap;
   final Map<String, dynamic> otherDataHashMap;
-  // NEW
   final String rawPoeJson;
   final int index;
   final WebSocketChannel channel;
@@ -27,39 +26,33 @@ class PoADetailsPage extends StatelessWidget {
   final pc.RSAPrivateKey privateKey;
   final String requestId;
 
-  const PoADetailsPage(
-      {super.key,
-      required this.proofType,
-      required this.publicKeyAlgorithm,
-      required this.publicKeyVerification,
-      required this.transferable,
-      required this.timestampFormat,
-      required this.timestampTime,
-      required this.gpsLat,
-      required this.gpsLng,
-      required this.gpsAlt,
-      required this.engagementEncoding,
-      required this.engagementData,
-      required this.sensitiveDataHashMap,
-      required this.otherDataHashMap,
-      // NEW
-      required this.rawPoeJson,
-      required this.index,
-      required this.channel,
-      required this.onPoERemove,
-      required this.privateKey,
-      required this.requestId});
+  const PoADetailsPage({
+    super.key,
+    required this.proofType,
+    required this.publicKeyAlgorithm,
+    required this.publicKeyVerification,
+    required this.transferable,
+    required this.timestampFormat,
+    required this.timestampTime,
+    required this.gpsLat,
+    required this.gpsLng,
+    required this.gpsAlt,
+    required this.engagementEncoding,
+    required this.engagementData,
+    required this.sensitiveDataHashMap,
+    required this.otherDataHashMap,
+    required this.rawPoeJson,
+    required this.index,
+    required this.channel,
+    required this.onPoERemove,
+    required this.privateKey,
+    required this.requestId,
+  });
 
-  /// Firma la PoE con la chiave privata e invia al `poe_client`
   void _approvePoE(BuildContext context) {
-    // Serializza la PoE e codificala in base64
-    //String poeBase64 = base64Encode(utf8.encode(rawPoeJson));
-
-    // Firma la PoE
     Uint8List signatureBytes = CryptoHelper.signJson(rawPoeJson, privateKey);
     String signatureBase64 = base64Encode(signatureBytes);
 
-    // Costruisci il messaggio
     final message = {
       "sourcePeer": "poe_es",
       "targetPeer": "poe_client",
@@ -70,19 +63,12 @@ class PoADetailsPage extends StatelessWidget {
       })))
     };
 
-    // Invia il messaggio al `poe_client`
     channel.sink.add(jsonEncode(message));
-
-    // Rimuove la PoE dalla lista
     onPoERemove(index);
-
-    // Torna alla pagina precedente
     Navigator.pop(context);
   }
 
-  /// Rifiuta la PoE e invia un messaggio al `poe_client`
   void _rejectPoE(BuildContext context) {
-    // Costruisci il messaggio di rifiuto
     final message = {
       "sourcePeer": "poe_es",
       "targetPeer": "poe_client",
@@ -90,13 +76,8 @@ class PoADetailsPage extends StatelessWidget {
           {"rejection": "PoE rifiutata", "public_key": publicKeyVerification})))
     };
 
-    // Invia il messaggio al `poe_client`
     channel.sink.add(jsonEncode(message));
-
-    // Rimuove la PoE dalla lista
     onPoERemove(index);
-
-    // Torna alla pagina precedente
     Navigator.pop(context);
   }
 
@@ -105,95 +86,91 @@ class PoADetailsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('PoE Details'),
+        title: const Text('Dettagli PoE'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context); // Torna alla pagina JsonPage
-          },
+          onPressed: () => Navigator.pop(context),
         ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
-          // Per gestire lo scroll in caso di contenuti lunghi
           child: Card(
-            elevation: 5, // Aggiunge un'ombra alla scheda
+            elevation: 5,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(12),
             ),
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'La PoE è valida!',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineLarge, // Stile del titolo
-                  ),
-                  const SizedBox(height: 20),
-                  Table(
-                    columnWidths: const {
-                      0: FlexColumnWidth(1), // Colonna delle chiavi
-                      1: FlexColumnWidth(2), // Colonna dei valori
-                    },
-                    border: TableBorder.all(
-                        color: Colors.grey.shade300), // Bordo della tabella
+                  Row(
                     children: [
-                      _buildTableRow('Proof Type', proofType),
-                      _buildTableRow(
-                          'Public Key Algorithm', publicKeyAlgorithm),
-                      _buildTableRow(
-                          'Public Verification Key', publicKeyVerification),
-                      _buildTableRow('Transferable', '$transferable'),
-                      _buildTableRow('Timestamp Format', timestampFormat),
-                      _buildTableRow('Timestamp Time', timestampTime),
-                      _buildTableRow('GPS Latitude', gpsLat.toString()),
-                      _buildTableRow('GPS Longitude', gpsLng.toString()),
-                      _buildTableRow('GPS Altitude', gpsAlt.toString()),
-                      _buildTableRow('Engagement Encoding', engagementEncoding),
-                      _buildTableRow('Engagement Data', engagementData),
-                      _buildTableRow('Engagement Data Decoded',
-                          utf8.decode(base64Decode(engagementData))),
+                      const Icon(Icons.verified, color: Colors.green, size: 28),
+                      const SizedBox(width: 10),
+                      Text(
+                        'PoE Valida!',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall!
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
                     ],
                   ),
                   const SizedBox(height: 20),
-                  Text(
-                    'Dati Sensibili:',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineSmall, // Stile dei dati sensibili
-                  ),
-                  const SizedBox(height: 10),
-                  _buildSensitiveDataTable(),
+                  _buildSectionTitle("Informazioni Generali"),
+                  _buildTable([
+                    _buildTableRow('Tipo di Prova', proofType),
+                    _buildTableRow(
+                        'Algoritmo Chiave Pubblica', publicKeyAlgorithm),
+                    _buildTableRow('Chiave di Verifica', publicKeyVerification),
+                    _buildTableRow('Trasferibile', transferable ? 'Sì' : 'No'),
+                    _buildTableRow('Formato Timestamp', timestampFormat),
+                    _buildTableRow('Orario Timestamp', timestampTime),
+                  ]),
                   const SizedBox(height: 20),
-                  Text(
-                    'Altri Dati:',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineSmall, // Stile degli altri dati
-                  ),
-                  const SizedBox(height: 10),
-                  _buildOtherDataTable(),
+                  _buildSectionTitle("Dati GPS"),
+                  _buildTable([
+                    _buildTableRow('Latitudine', gpsLat.toString()),
+                    _buildTableRow('Longitudine', gpsLng.toString()),
+                    _buildTableRow('Altitudine', gpsAlt.toString()),
+                  ]),
+                  const SizedBox(height: 20),
+                  _buildSectionTitle("Dati di Engagement"),
+                  _buildTable([
+                    _buildTableRow('Codifica', engagementEncoding),
+                    _buildTableRow('Dati (Base64)', engagementData),
+                    _buildTableRow('Dati Decodificati',
+                        utf8.decode(base64Decode(engagementData))),
+                  ]),
+                  const SizedBox(height: 20),
+                  _buildSectionTitle("Dati Sensibili"),
+                  _buildDataTable(sensitiveDataHashMap),
+                  const SizedBox(height: 20),
+                  _buildSectionTitle("Altri Dati"),
+                  _buildDataTable(otherDataHashMap),
                   const SizedBox(height: 20),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      ElevatedButton(
+                      ElevatedButton.icon(
                         onPressed: () => _approvePoE(context),
+                        icon: const Icon(Icons.check, color: Colors.white),
+                        label: const Text("Approva PoE"),
                         style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green,
-                            textStyle: const TextStyle(color: Colors.black87)),
-                        child: const Text("Approva PoE"),
+                          backgroundColor: Colors.green,
+                          foregroundColor: Colors.white,
+                        ),
                       ),
-                      ElevatedButton(
+                      ElevatedButton.icon(
                         onPressed: () => _rejectPoE(context),
+                        icon: const Icon(Icons.cancel, color: Colors.white),
+                        label: const Text("Rifiuta PoE"),
                         style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.red,
-                            textStyle: const TextStyle(color: Colors.black87)),
-                        child: const Text("Rifiuta PoE"),
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                        ),
                       ),
                     ],
                   ),
@@ -206,54 +183,46 @@ class PoADetailsPage extends StatelessWidget {
     );
   }
 
-  // Funzione per creare una riga della tabella
+  Widget _buildSectionTitle(String title) {
+    return Text(
+      title,
+      style: const TextStyle(
+          fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blueAccent),
+    );
+  }
+
+  Widget _buildTable(List<TableRow> rows) {
+    return Table(
+      columnWidths: const {0: FlexColumnWidth(1), 1: FlexColumnWidth(2)},
+      border: TableBorder.all(color: Colors.grey.shade300),
+      children: rows,
+    );
+  }
+
   TableRow _buildTableRow(String key, String value) {
     return TableRow(
       children: [
-        Padding(
+        Container(
+          color: Colors.grey.shade200,
           padding: const EdgeInsets.all(8.0),
           child: Text(
             key,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
+            style: const TextStyle(fontWeight: FontWeight.bold),
           ),
         ),
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Text(
-            value,
-            style: const TextStyle(fontSize: 16),
-          ),
+          child: SelectableText(value),
         ),
       ],
     );
   }
 
-  // Tabella per i dati sensibili
-  Widget _buildSensitiveDataTable() {
+  Widget _buildDataTable(Map<String, dynamic> data) {
     return Table(
-      columnWidths: const {
-        0: FlexColumnWidth(1),
-        1: FlexColumnWidth(2),
-      },
+      columnWidths: const {0: FlexColumnWidth(1), 1: FlexColumnWidth(2)},
       border: TableBorder.all(color: Colors.grey.shade300),
-      children: sensitiveDataHashMap.entries.map((entry) {
-        return _buildTableRow(entry.key, entry.value);
-      }).toList(),
-    );
-  }
-
-  // Tabella per altri dati
-  Widget _buildOtherDataTable() {
-    return Table(
-      columnWidths: const {
-        0: FlexColumnWidth(1),
-        1: FlexColumnWidth(2),
-      },
-      border: TableBorder.all(color: Colors.grey.shade300),
-      children: otherDataHashMap.entries.map((entry) {
+      children: data.entries.map((entry) {
         return _buildTableRow(entry.key, entry.value.toString());
       }).toList(),
     );
